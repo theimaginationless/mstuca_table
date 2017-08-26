25d7fc69e7a9f48e5fb1c57a7be8bb21289e798e
Dmitry Teplyakov <dimon2242@gmail.com>
2017-08-26 21:18:45 +0300

    Initial preferences support

---
+++ app/src/main/java/com/app/dmitryteplyakov/shedule/PrefActivity.java
@@ -0,0 +1,8 @@
+package com.app.dmitryteplyakov.shedule;
+
+/**
+ * Created by dmitry21 on 26.08.17.
+ */
+
+public class PrefActivity {
+}
---
+++ app/src/main/java/com/app/dmitryteplyakov/shedule/PrefFragment.java
@@ -0,0 +1,16 @@
+package com.app.dmitryteplyakov.shedule;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by dmitry21 on 26.08.17.
+ */
+
+public class PrefFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String s) {
+
+    }
+}
